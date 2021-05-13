333b890d7999cd9c3ee0ccc4b14442db326a662e
DuhanMo <devduhanmo@gmail.com>
2021-05-13 18:38:27 +0900

    design pattern : proxy, decorator

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,13 +1,20 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.*;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
@@ -17,6 +24,8 @@ public class Main {
 
         System.out.println(aClient.equals(bClient));
         */
+
+        /*
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
@@ -27,6 +36,62 @@ public class Main {
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V airAdapter = new SocketAdapter(airConditioner);
         connect(airAdapter);
+        */
+
+        // proxy pattern
+        /*
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+         */
+        /*
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+
+         */
+/*
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                });
+
+        aopBrowser.show();
+        System.out.println("loading time: " + end.get());       aopBrowser.show();
+        aopBrowser.show();
+        System.out.println("loading time: " + end.get());
+
+ */
+        // decorator 패턴
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        //a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+        //a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+
+        //a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();
+
+
     }
 
     // 콘센트
