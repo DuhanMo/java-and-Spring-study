b9982adf9803abd2dcb82a28009262713f3dec47
DuhanMo <devduhanmo@gmail.com>
2021-06-03 22:42:11 +0900

    Server(Client) to Server 통신 RestTemplate PostMapping

--- client/src/main/java/com/example/client/controller/ApiController.java
+++ client/src/main/java/com/example/client/controller/ApiController.java
@@ -20,6 +20,7 @@ public class ApiController {
 
     @GetMapping("/hello")
     public UserResponse getHello() {
-        return restTemplateService.hello();
+//        return restTemplateService.hello();
+        return restTemplateService.post();
     }
 }
--- client/src/main/java/com/example/client/service/RestTemplateService.java
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,5 +1,6 @@
 package com.example.client.service;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -37,11 +38,43 @@ public class RestTemplateService {
 
 
         // 그러면 json형태는 어떻게 받는가?
+        // getForEntity 는 가져오겠다의 get 이 아니라 HttpMethod의 Get 메소드임
         ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
 
         System.out.println(result.getStatusCode()); // res의 상태코드
         System.out.println(result.getBody()); // res의 내용
         return result.getBody();
 
+    }
+
+    public UserResponse post() {
+
+        // http://localhost:9090/api/server/user/{userId}/name/{userName} --> user를 등록시키는 uri 예제일뿐 실제로
+        // 이렇게 사용하진 않음
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "postSteve") // 위 {} 에 자동으로 매핑됨
+                .toUri();
+        System.out.println(uri);
+        // Post 이기때문에 Http Body가 있어야 함
+        // object -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("james2");
+        req.setAge(97);
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // uri, 보내고싶은 객체와 받고싶은 타입
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req,UserResponse.class);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+
+        return response.getBody();
     }
 }
