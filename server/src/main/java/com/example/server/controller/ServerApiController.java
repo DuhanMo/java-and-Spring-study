46d0a6e0dbe03d4ed548f45b755a86c3986a1948
DuhanMo <devduhanmo@gmail.com>
2021-06-04 17:20:03 +0900

    naver api 검색-지도 사용

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -2,12 +2,38 @@ package com.example.server.controller;
 
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+    @GetMapping("/naver")
+    public String naver() {
+        // https://openapi.naver.com/v1/search/local.json
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com/v1/search/local.json")
+                .queryParam("query","갈비집")
+                .encode()
+                .build()
+                .toUri();
+        System.out.println(uri);
+        RequestEntity<Void> requestEntity = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","YCkJq1wijWw3hK33uw92")
+                .header("X-Naver-Client-Secret","MeTatAqYYm")
+                .build();
+        System.out.println(requestEntity);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.exchange(requestEntity,String.class);
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name,
